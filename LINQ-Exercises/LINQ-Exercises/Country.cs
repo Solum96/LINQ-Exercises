95c686e23688f9f01698d44feacf9bd24cb19284
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-14 12:00:16 +0200

    Added country class and created List<Country>
    Didn't use all suggested countries

---
+++ LINQ-Exercises/LINQ-Exercises/Country.cs
@@ -0,0 +1,18 @@
+﻿namespace LINQ_Exercises
+{
+    internal class Country
+    {
+        public string name;
+        public string capital;
+        public double population;
+        public int area;
+
+        public Country(string name, string capital, double population, int area)
+        {
+            this.name = name;
+            this.capital = capital;
+            this.population = population;
+            this.area = area;
+        }
+    }
+}
--- LINQ-Exercises/LINQ-Exercises/Program.cs
+++ LINQ-Exercises/LINQ-Exercises/Program.cs
@@ -10,6 +10,22 @@ namespace LINQ_Exercises
     {
         static void Main(string[] args)
         {
+            List<Country> countries = new List<Country>();
+            Country sweden = new Country("Sverige", "Stockholm", 10.07, 450295);
+            Country norway = new Country("Norge", "Oslo", 5.27, 323802);
+            Country iceland = new Country("Island", "Reykyavik", 0.33, 102775);
+            Country denmark = new Country("Danmark", "Köpenhamn", 5.75, 42931);
+            Country finland = new Country("Finland", "Helsingfors", 5.51, 338424);
+            Country belgium = new Country("Belgien", "Bryssel", 11.30, 30528);
+
+            countries.Add(sweden);
+            countries.Add(norway);
+            countries.Add(iceland);
+            countries.Add(denmark);
+            countries.Add(finland);
+            countries.Add(belgium);
+
+
         }
     }
 }
