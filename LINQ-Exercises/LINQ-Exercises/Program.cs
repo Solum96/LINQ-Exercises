70b7720371f166951ba2630a69c7c949add8c56d
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-15 10:53:10 +0200

    Added exercise 6

--- LINQ-Exercises/LINQ-Exercises/Program.cs
+++ LINQ-Exercises/LINQ-Exercises/Program.cs
@@ -17,6 +17,7 @@ namespace LINQ_Exercises
             Country denmark = new Country("Danmark", "Köpenhamn", 5.75, 42931);
             Country finland = new Country("Finland", "Helsingfors", 5.51, 338424);
             Country belgium = new Country("Belgien", "Bryssel", 11.30, 30528);
+            Country germany = new Country("Tyskland", "Berlin", 82.18, 357168);
 
             countries.Add(sweden);
             countries.Add(norway);
@@ -24,6 +25,7 @@ namespace LINQ_Exercises
             countries.Add(denmark);
             countries.Add(finland);
             countries.Add(belgium);
+            countries.Add(germany);
 
             /* Övning 2
                 var query = from country in countries
@@ -63,6 +65,20 @@ namespace LINQ_Exercises
                         select country;
 
             Console.WriteLine(query.First().population); */
+
+            /* ÖVNING 6
+            var query = from country in countries
+                        select country.area;
+
+            var query2 = from country in countries
+                         where country.area < query.Average() // Kan bytas ut mot countries.Average((c) => c.area)
+                         select country;
+
+            Console.WriteLine(query.Average());
+            foreach (var item in query2)
+            {
+                Console.WriteLine(item.name);
+            } */
 
 
             Console.ReadKey(); 
